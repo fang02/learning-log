9d98ff0309243fdae67745318cf30197f74231e9
Dhrumil Mistry <dhrumilmistry01@gmail.com>
2026-09-14 15:40:32 +1000

    deep question but useless

---
+++ deep/deep.py
@@ -0,0 +1,9 @@
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").strip().lower()
+if answer == "42":
+    print("Yes")
+elif answer == "forty-two":
+    print("Yes")
+elif answer == "forty two":
+    print("Yes")
+else:
+    print("No")
